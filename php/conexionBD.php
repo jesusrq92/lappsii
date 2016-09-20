aff32a33d4ce301c6c9d18c0b98f578c25321e9b
Jesús Ricaño <racing_rica@hotmail.com>
2016-09-20 13:46:16 -0500

    base de datos habilitada

---
+++ php/conexionBD.php
@@ -0,0 +1,8 @@
+<?php
+
+	$server="mysql.hostinger.mx";
+	$user="u204657844_user";
+	$pass="lappsii";
+	$db="u204657844_db";
+
+?>
--- php/contactar2.php
+++ php/contactar2.php
@@ -2,16 +2,17 @@
 
 header("Content-Type: text/html; charset=utf-8");
 
+include("conexionBD.php");
+
 //Obtenemos valores del formulario
 $namePHP = $_POST['name'];
 $emailPHP = $_POST['email'];
 $companyPHP = $_POST['company'];
 $messagePHP = $_POST['message'];
 
-$copyEmailPHP = "contacto@lappsii.com";
 
-//variables del Correo
-$destino = "$copyEmailPHP,$emailPHP";
+//Variables del Correo
+$destino  = "$emailPHP";
 
 $asunto = "Mensaje de Lappsii";
 
@@ -24,14 +25,34 @@ $template = str_replace('\r\n','<br>', $template);
 
 
 $header  = "From: Lappsii <contacto@lappsii.com>\r\n";
+$header .= "Bcc: racing_rica@hotmail.com\r\n";
 $header .= "Reply-To: " .$emailPHP. "\r\n";
 $header .= "Content-Type: text/html\r\n";
+
+
+//Insercion a la BD
+if(isset($_POST['name']) && !empty($_POST['name']) &&
+   isset($_POST['email']) && !empty($_POST['email']) &&
+   isset($_POST['company']) && !empty($_POST['company']) &&
+   isset($_POST['message']) && !empty($_POST['message']))
+{
+  $link = mysqli_connect($server, $user, $pass) or die('Problemas al conectar');
+  mysqli_select_db($link, $db) or die('Problemas al conectar a la BD');
+
+  mysqli_query($link, "INSERT INTO clientes (name, email, company, message) values ('$namePHP', '$emailPHP', '$companyPHP', '$messagePHP')");
+
+  mysqli_close($link);
+
+} else {
+  echo "Problemas al insertar a la Base de Datos";
+}
         
 
 //Envio del correo
 $enviado = mail($destino, $asunto, $template, $header);
 
 if($enviado){
+
     echo '
           <div id="DivForm">
             <script>document.getElementById("lappsiiForm2").reset();</script>
@@ -46,6 +67,7 @@ if($enviado){
 
           </div>
          ';
+
 } else {
 	  echo '
           <div id="DivForm">
